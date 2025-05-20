27733a4321069f1337e4f9f2509794f174ad058b
Dasari Uhaa <dasariuhaa@gmail.com>
2025-05-20 21:22:11 +0530

    itial commit

---
+++ demoUhaa/src/main/java/com/exampleUhaa/demoUhaa/DemoUhaaApplication.java
@@ -0,0 +1,13 @@
+package com.exampleUhaa.demoUhaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUhaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUhaaApplication.class, args);
+	}
+
+}
---
+++ demoUhaa/src/test/java/com/exampleUhaa/demoUhaa/DemoUhaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleUhaa.demoUhaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUhaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
